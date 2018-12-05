61bbbb3b6e3f35e67e9afb5bf5bc7436520b01c8
heexbot <39099888+heexbot@users.noreply.github.com>
2018-12-05 14:13:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var adminprefix = "L";
-const developers = ["441554686399741962","323888904602124299"]
-client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!developers.includes(message.author.id)) return;
-      
-  if (message.content.startsWith(adminprefix + 'setg')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**تم تغيير حالتك الى** **${argresult}**`)
-  } else 
-     if (message.content === (adminprefix + "leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith(adminprefix + 'setw')) {
-  client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**تم تغيير الواتشنق الى** **${argresult}**`)
-  } else 
-  if (message.content.startsWith(adminprefix + 'setl')) {
-  client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**تم تغير اللستنق الى ** **${argresult}**`)
-  } else 
+var adminprefix = "i";
+const developers = ["425682014931517445","323888904602124299"]
+
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
       message.channel.send(`**تم تغيير الستريمنق الى** **${argresult}**`)
   }
-  if (message.content.startsWith(adminprefix + 'setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`**تم تغيير اسمك الى ** ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`**تم تغيير صورتك الى**:**${argresult}** `);
+ 
 }
 });
 
